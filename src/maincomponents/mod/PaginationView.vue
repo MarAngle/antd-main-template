23a2c317ebd4c2b6786817ce682a9d45b4430ff9
MarAngle <yinweiyingzhe@126.com>
2021-03-26 09:19:02 +0800

    优化分页器布局，准备继续选择器的实现，分页器功能待定

--- src/maincomponents/mod/PaginationView.vue
+++ src/maincomponents/mod/PaginationView.vue
@@ -4,9 +4,10 @@
 <template>
   <div
     class="mainpagination"
+    v-bind="currentMainOption.props"
   >
     <a-pagination
-      v-bind="paginationOption.props"
+      v-bind="currentOption.props"
       @change="onChange"
       @showSizeChange="onSizeChange"
     />
@@ -28,27 +29,50 @@
         type: Object,
         required: false,
         default: null
+      },
+      mainOption: {
+        type: Object,
+        required: false,
+        default: null
       }
     },
     computed: {
-      paginationOption() {
-        let paginationOption = this.data.getOption()
+      currentOption() {
+        let currentOption = this.data.getOption()
         if (this.option) {
           for (let n in this.option) {
-            if (!paginationOption[n]) {
-              paginationOption[n] = {}
+            if (!currentOption[n]) {
+              currentOption[n] = {}
             }
-            paginationOption[n] = {
-              ...paginationOption[n],
+            currentOption[n] = {
+              ...currentOption[n],
               ...this.option[n]
             }
           }
         }
-        paginationOption.props.current = this.data.data.page.current
-        paginationOption.props.pageSize = this.data.data.size.current
-        paginationOption.props.pageSizeOptions = this.data.data.size.list
-        paginationOption.props.total = this.data.data.num.total
-        return paginationOption
+        currentOption.props.current = this.data.data.page.current
+        currentOption.props.pageSize = this.data.data.size.current
+        currentOption.props.pageSizeOptions = this.data.data.size.list
+        currentOption.props.total = this.data.data.num.total
+        return currentOption
+      },
+      currentMainOption() {
+        let currentMainOption = {
+          props: {
+            style: {
+              padding: '10px 0'
+            }
+          }
+        }
+        if (this.mainOption) {
+          for (let n in this.mainOption) {
+            currentMainOption.props[n] = {
+              ...currentMainOption[n],
+              ...this.mainOption[n]
+            }
+          }
+        }
+        return currentMainOption
       }
     },
     methods: {
