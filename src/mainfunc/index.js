1699fc2b1600b0571636b689eff489965fdb9cf7
MarAngle <yinweiyingzhe@126.com>
2021-04-26 16:53:20 +0800

    注释测试

--- src/mainfunc/index.js
+++ src/mainfunc/index.js
@@ -4,7 +4,7 @@ import _rule from './data/rule'
 import _utils from './data/utils'
 import _notice from '@/mainnotice/index'
 
-import './test/deepClone'
+// import './test/deepClone'
 
 let mainfunc = {
   BASEDATA: {}
