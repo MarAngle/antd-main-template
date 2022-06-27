c81d3a17b9e53d835df1c9f153aa9539c08683d5
MarAngle <yinweiyingzhe@126.com>
2022-06-27 10:49:49 +0800

    版本依赖升级

--- src/maindata/index.js
+++ src/maindata/index.js
@@ -3,7 +3,7 @@ import 'complex-component-antd'
 // 功能插件
 import './func/index'
 // 实现类
-import { antdOption } from 'complex-data'
+import antdOption from 'complex-data/option-ant/index'
 
 // 实现类antd-option加载
 antdOption.init()
